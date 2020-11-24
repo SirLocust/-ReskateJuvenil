11de5297576959470fcef4f85d634e881658aa87
sirlocust <jua56yu@hotmail.com>
2020-11-24 01:01:26 -0500

    fix template html

--- src/app/share/components/footer/footer.component.ts
+++ src/app/share/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary} from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faMapMarker, faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
@@ -9,22 +9,6 @@ import { faEnvelope, faMapMarker, faPhoneVolume } from '@fortawesome/free-solid-
 export class FooterComponent implements OnInit {
 
   constructor(public iconLibrary: FaIconLibrary) { }
-  itemsList = [
-
-    {
-      data: 'reskatejuvenil@gmail.com',
-      icon: ['fas', 'envelope']
-    },
-    {
-      data: 'Cra. 71g #6D-44, Bogotá',
-      icon: ['fas', 'map-marker']
-    },
-    {
-      data: '311 8837156',
-      icon: ['fas','phone-volume']
-    }
-  
-  ]
   ngOnInit(): void {
     this.iconLibrary.addIcons(
       faEnvelope,
